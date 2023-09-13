d31460e295040c2dceb83d9d299a4a0062714eb2
Iago-Santos-Sousa <iago.santos.sousa@gmail.com>
2023-09-12 22:12:39 -0300

    cm-07

--- src/components/start/Start.jsx
+++ src/components/start/Start.jsx
@@ -8,7 +8,10 @@ const Start = () => {
           <div className="start-text-container">
             <h3>Oi, eu sou</h3>
             <h2>Iago Dos Santos Sousa</h2>
-            <h3>Desenvolvedor Front-End</h3>
+            <h3>
+              Desenvolvedor Front-Ends
+              <span className="cursor-blink">|</span>
+            </h3>
             <a
               href="https://www.linkedin.com/in/iagosantossousa/"
               target="_blank"
